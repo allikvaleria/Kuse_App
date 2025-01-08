8232a78a102c3a97f7fdcd5bdc8647294501b959
opilane <opilane@HP-CZC2349HTZ>
2025-01-08 11:36:15 +0200

    Add project files.

---
+++ Kuse_App/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kuse_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Ootan sind minu peole! Palun tule!!!";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 10 ? "Tere hommikust!" : "Tere päevast!";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Ankeet()
+        {
+            return View();
+        }
+    }
+}
---
+++ Kuse_App/Models/Guest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Kuse_App.Models
+{
+    public class Guest
+    {
+        [Required(ErrorMessage = "Sisesta nimi")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Sisesta email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Valesti sisestatud email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Sisesta telefoni number")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Numbri alguses peal olema +372")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Sisesta oma valik")]
+        public bool? WillAttend { get; set; }
+    }
+}
